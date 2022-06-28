e942ddbeb19a9b7f49f1152335b7ad5dde4bbe88
Daniel Bark <danba340@gmail.com>
2022-06-28 10:11:19 +0200

    bumped all versions and use new react-three apis

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stars } from "@react-three/drei";
+import { Physics, usePlane, useBox } from "@react-three/cannon";
+import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+function Box() {
+	const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+	return (
+		<mesh
+			onClick={() => {
+				api.velocity.set(0, 2, 0);
+			}}
+			ref={ref}
+			position={[0, 2, 0]}
+		>
+			<boxBufferGeometry attach="geometry" />
+			<meshLambertMaterial attach="material" color="hotpink" />
+		</mesh>
+	);
+}
+
+function Plane() {
+	const [ref] = usePlane(() => ({
+		rotation: [-Math.PI / 2, 0, 0],
+	}));
+	return (
+		<mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
+			<planeBufferGeometry attach="geometry" args={[100, 100]} />
+			<meshLambertMaterial attach="material" color="lightblue" />
+		</mesh>
+	);
+}
+
+createRoot(document.getElementById('root')).render(
+	<Canvas>
+		<OrbitControls />
+		<Stars />
+		<ambientLight intensity={0.5} />
+		<spotLight position={[10, 15, 10]} angle={0.3} />
+		<Physics>
+			<Box />
+			<Plane />
+		</Physics>
+	</Canvas>
+);
